ec5639bd9e05c33dff372c42d9de4e28f18f5fd1
GamingInfinite <ender2gamer123@gmail.com>
2022-06-18 14:15:27 -0500

    firebase setup stuff
    idk

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,56 @@
 <script lang="ts">
+  import { Card, Deck } from "./util.js";
+//   import { initializeApp } from "firebase/app";
+//   import { getAnalytics } from "firebase/analytics";
 
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyC_hobr42NEOA46_O-YWYSoXfZgQvTIJa0",
+//     authDomain: "tabletalkproj.firebaseapp.com",
+//     projectId: "tabletalkproj",
+//     storageBucket: "tabletalkproj.appspot.com",
+//     messagingSenderId: "550808021827",
+//     appId: "1:550808021827:web:88c295a1c72ff68b885df6",
+//     measurementId: "G-ER3VK8XS20",
+//   };
 
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
+
+  var testDeck = new Deck(true);
+  testDeck.shuffle();
 </script>
 
-<div>
-    <div class="title">
-        bruh
-    </div>
+<div class="titleScreen">
+  <div class="title">TableTalk</div>
+  <div class="buttons">
+    <button class="playButton">Play</button>
+  </div>
 </div>
 
 <style>
+  @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap");
+
+  .titleScreen {
+    background-color: rgb(12, 171, 199);
+    height: 100vh;
+    text-align: center;
+    font-family: "Roboto", sans-serif;
+    font-size: 100px;
+  }
+
+  .title {
+    padding-top: 10vh;
+    padding-bottom: 10vh;
+  }
+
+  .playButton {
+    background-color: green;
+    border: none;
+    text-align: center;
+    width: 20vw;
+    font-family: "Roboto", sans-serif;
+    font-size: 50px;
+    color: black;
+    border-radius: 20px;
+  }
 </style>
--- src/util.ts
+++ src/util.ts
@@ -1,62 +1,69 @@
 export class Card {
-    suit: string;
-    value: number;
+  suit: string;
+  value: number;
 
-    constructor() {
-        
-    }
-
-    constructor(suit: string, value: number) {
-        this.suit = suit;
-        this.value = value;
-    }
+  constructor(suit: string, value: number) {
+    this.suit = suit;
+    this.value = value;
+  }
 }
 
 enum Suits {
-    CLUBS = "Clubs",
-    HEARTS = "Hearts",
-    SPADES = "Spades",
-    DIAMONDS = "Diamonds",
-    JOKER = "Joker"
+  CLUBS = "Clubs",
+  HEARTS = "Hearts",
+  SPADES = "Spades",
+  DIAMONDS = "Diamonds",
+  JOKER = "Joker",
 }
 
 enum CardTypes {
-    ACE = 1,
-    TWO = 2,
-    THREE = 3,
-    FOUR = 4,
-    FIVE = 5,
-    SIX = 6,
-    SEVEN = 7,
-    EIGHT = 8,
-    NINE = 9,
-    TEN = 10,
-    JACK = 11,
-    QUEEN = 12,
-    KING = 13,
-    JOKER = 0,
+  ACE = 1,
+  TWO = 2,
+  THREE = 3,
+  FOUR = 4,
+  FIVE = 5,
+  SIX = 6,
+  SEVEN = 7,
+  EIGHT = 8,
+  NINE = 9,
+  TEN = 10,
+  JACK = 11,
+  QUEEN = 12,
+  KING = 13,
+  JOKER = 0,
 }
 
 export class Deck {
-    deck: Array<Card>;
+  deck: Card[];
 
-    constructor(decks: number) {
-        this.deck = []
-        for (let i = 1; i < 14; i++) {
-            this.deck.push(new Card(Suits.CLUBS), i)
-            this.deck.push(new Card(Suits.HEARTS), i)
-            this.deck.push(new Card(Suits.DIAMONDS), i)
-            this.deck.push(new Card(Suits.SPADES), i)
-        }
-        this.deck.push(new Card(Suits.JOKER, 0))
-        this.deck.push(new Card(Suits.JOKER, 0))
+  constructor(jokers?: boolean) {
+    this.deck = [];
+    for (let i = 1; i < 14; i++) {
+      this.deck.push(new Card(Suits.HEARTS, i));
+      this.deck.push(new Card(Suits.DIAMONDS, i));
+      this.deck.push(new Card(Suits.SPADES, i));
+      this.deck.push(new Card(Suits.CLUBS, i));
     }
 
-    constructor(deck: Array) {
-        this.deck = deck;
+    if (jokers) {
+      this.deck.push(new Card(Suits.JOKER, 0));
+      this.deck.push(new Card(Suits.JOKER, 0));
+    }
+  }
+
+  shuffle() {
+    var randDeck: Array<Card> = [];
+    while (this.deck.length > 0) {
+      var randPick: number = Math.floor(Math.random() * this.deck.length);
+      randDeck.push(this.deck[randPick]);
+      this.deck.splice(randPick, 1);
     }
 
-    shuffle() {
+    this.deck = randDeck;
 
+    for (let i = 0; i < this.deck.length; i++) {
+      const element = this.deck[i];
+      console.log(element.suit, " ", element.value);
     }
+  }
 }
